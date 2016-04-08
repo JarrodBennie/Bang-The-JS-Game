847567b0c84db4e23b5212ffa0f67802fcb2ec60
Adam Reid <adamreid21@hotmail.co.uk>
2016-04-08 19:22:35 +0100

    dice

--- client/src/bang_game/specs/dice_spec.js
+++ client/src/bang_game/specs/dice_spec.js
@@ -8,8 +8,12 @@ describe('Dice', function(){
     dice = new Dice();
   });
 
-  it("should return random number", function(){
-    assert.equal(dice.roll, 7 );
+  it("should return random number between 1-6", function(){
+    assert.notEqual(dice.roll(1), 7 );
+  });
+
+  it("should return array of 5 random numbers between 1-6", function(){
+    assert.equal(dice.roll(5).length, 5);
   });
 
 
