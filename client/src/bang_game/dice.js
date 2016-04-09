64ae707534adb59b5c949a6fd7319eaa0b2d4e2a
Adam Reid <adamreid21@hotmail.co.uk>
2016-04-09 02:41:52 +0100

    can save dice and re-roll remaining dice, dynamite automatically saves so cant re-roll it, function to return true if 3 dynamite, function to return true if 3 gatling

--- client/src/bang_game/dice.js
+++ client/src/bang_game/dice.js
@@ -1,4 +1,8 @@
 var Dice = function(){
+  this.currentRoll = [];
+  this.saved = [];
+  //// will need to reset dice.saved to [] between change of player turn.
+
   this.meaningOf = {
     1: "Shoot 1",
     2: "Shoot 2",
@@ -10,13 +14,66 @@ var Dice = function(){
 };
 
 
-Dice.prototype.roll = function(numberOfDice){
-  var rollsArray = [];
-  for( var i=0; i<numberOfDice; i++){
+Dice.prototype.roll = function(){
+  this.currentRoll = [];
+  var numberOfDiceToRoll = 5 - this.saved.length;
+  for( var i=0; i < numberOfDiceToRoll; i++){
     var result = Math.floor(Math.random() * 6) + 1;
-    rollsArray.push( result );
+    this.currentRoll.push( result );
   };
-  return rollsArray;
+  this.saveDynamite();
+  return this.currentRoll;
 };
+//// for special cards could add in above: if( playerSpecialAbility != [the special ability that lets you re-roll dynamite]){ this.saveDynamite } so save dynamite happens to everyone except the player with the special card. but it wont know what player - so would have to pass in the player object - dice.save(player1) seems a bit ugly but would allow us to check player special card.
+
+Dice.prototype.save = function( index ){
+  this.saved.push( this.currentRoll[ index ]);
+};
+
+Dice.prototype.saveDynamite = function(){
+  for( var item of this.currentRoll){
+    if( item === 5){
+      var indexOfDynamite = this.currentRoll.indexOf( item );
+      this.save( indexOfDynamite );
+    };
+  };
+};
+
+
+//// could use dice.currentRoll and dice.saved and loop through each checking if 3 dynamite, 3 gatling, and how many arrows. Return true if 3 dynamite/gatling.  In game can do if(dice.threeDynamite){ the run the function to take life off player and run the function to end player turn/start new player turn }    ----  could also do if(dice.threeGatling){ shoot all players & set current player arrows = 0 }.
+Dice.prototype.threeDynamite = function(){
+  var result = false;
+  var counter = 0;
+  for( var number of this.currentRoll){
+    if( number === 5){
+      counter ++;
+    };
+  };
+  for( var number of this.saved){
+    if( number === 5){
+      counter ++;
+    };
+  };
+  if( counter >= 3){
+    result = true;
+  };
+  return result;
+};
+
+Dice.prototype.threeGatling = function(){
+  var result = false;
+  var counter = 0;
+  for( item of this.saved){
+    if( item === 4 ){
+      counter++;
+    };
+  };
+  if(counter >= 3){
+    result = true;
+  };
+  return result;
+};
+
+
 
 module.exports = Dice;
--- client/src/bang_game/specs/dice_spec.js
+++ client/src/bang_game/specs/dice_spec.js
@@ -1,20 +1,56 @@
 var assert = require('chai').assert;
 var Dice = require('../dice.js');
 
-var dice;
 
 describe('Dice', function(){
+  var dice;
+  var testRoll;
   beforeEach(function(){
     dice = new Dice();
+    dice.currentRoll = [ 1, 2, 3, 5, 6 ]
+  });
+
+  it("should have an array of saved Dice", function(){
+    assert.deepEqual( dice.saved, []);
   });
 
   it("should return random number between 1-6", function(){
-    assert.notEqual(dice.roll(1), 7 );
+    assert.notEqual( dice.roll(), 7);
   });
 
-  it("should return array of 5 random numbers between 1-6", function(){
-    assert.equal(dice.roll(5).length, 5);
+  it("should return array of 5 random numbers", function(){
+    assert.equal(dice.roll().length, 5);
   });
+
+  it("should save dice at index 2", function(){
+    dice.save(2);
+    assert.deepEqual( dice.saved, [ 3 ] );
+  });
+
+  it("should save dynamite automatically from dice current roll", function(){
+    dice.saveDynamite();
+    assert.deepEqual( dice.saved, [ 5 ]);
+  });
+
+  it("should return true if 3 or more dynamite in current roll", function(){
+    dice.currentRoll = [ 5, 5, 1, 5, 2 ];
+    assert.equal(dice.threeDynamite(), true);
+  });
+
+  it("should return true if 3 or more dynamite combined between current roll and saved", function(){
+    dice.save(3);
+    dice.currentRoll = [ 5, 1, 2, 3, 5 ];
+    assert.equal(dice.threeDynamite(), true);
+  });
+
+  it("should return true if 3 or more gatling saved", function(){
+    dice.currentRoll = [ 4, 4, 4, 1, 2 ];
+    dice.save(0);
+    dice.save(1);
+    dice.save(2);
+    assert.equal(dice.threeGatling(), true);
+  });
+
 
 
 
