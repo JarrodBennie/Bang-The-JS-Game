4f35aa392f9cdb451770c25f5230c3e3df7666fd
Craig Morton <CraigMorton92@gmail.com>
2016-04-11 00:23:37 +0100

    quick fix hint spec

--- client/src/bang_game/specs/hint_spec.js
+++ client/src/bang_game/specs/hint_spec.js
@@ -1,6 +1,12 @@
+var assert = require('chai').assert;
 var Hint = require('../hint');
 
+var hint;
+
 describe('Hint', function() {
+  beforeEach(function(){
+    hint = new Hint();
+  });
   it('should have an array of 4 hints', function(){
     assert.equal( hint.all.length, 4 )
   });
