4f59434619d494e3cd9a72ead334242acaaef3be
Adam Reid <adamreid21@hotmail.co.uk>
2016-04-14 10:19:36 +0100

    adds -please resolve your dice- toast

--- client/src/index.js
+++ client/src/index.js
@@ -499,7 +499,7 @@ window.onload = function(){
     diceClickEnable();
     rollDice();
 
-    if(dice.canRoll === false){
+    if(dice.canRoll() === false){
       this.onclick = null;
       rollDiceButton.setAttribute('class', 'waves-effect waves-light btn disabled');
       game.addToActionCounters();
@@ -732,7 +732,8 @@ var enableRollDiceButton = function(rollDiceButton, game){
     diceClickEnable();
     rollDice(dice, diceElements, game);
 
-    if(dice.canRoll === false){
+    if(dice.canRoll() === false){
+      //maybe re-add duplicate "use all dice to end turn" toast
       this.onclick = null;
       rollDiceButton.setAttribute('class', 'waves-effect waves-light btn disabled');
       game.addToActionCounters();
@@ -835,6 +836,7 @@ var endGame = function(gameResult){
 
 var savedDiceFull = function(){
   if(dice.canRoll() === false){
+    Materialize.toast("Resolve all dice to enable End Turn button", 3500)
     for (var i = 0; i < diceElements.length; i++) diceElements[i].style.opacity = 1;
       rollDiceButton.setAttribute('class', 'waves-effect waves-light btn disabled');
     game.addToActionCounters();
