ed2e584e5f4bff8d0d73d230d39c455549693c4a
JarrodBennie <jarrodbennie@icloud.com>
2016-12-20 11:07:12 +0000

    removes unused mongo/bp stuff from server.js

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const MongoClient = require('mongodb').MongoClient;
-const bodyParser = require('body-parser');
-const url = 'mongodb://localhost:27017/databaseName';
 
-app.use(bodyParser.json());
 app.use(express.static('client/build'));
 
-app.get( '/', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(`${ __dirname }/client/build/index.html`));
 });
 
@@ -22,36 +18,6 @@ app.get('/rules', (req, res) => {
 
 app.get('/new', (req, res) => {
   res.sendFile(path.join(`${ __dirname }/client/build/index.html`));
-});
-
-app.get('/name', (req, res) => {
-  MongoClient.connect(url, (err, db) => {
-    if (err) {
-      console.err(err);
-      return;
-    }
-
-    const collection = db.collection('name');
-    collection.find({}).toArray((err, docs) => {
-      res.json(docs);
-      db.close();
-    });
-  });
-});
-
-app.post('/name', (req, res) => {
-  MongoClient.connect(url, (err, db) => {
-    if (err) {
-      console.err(err);
-      return;
-    }
-
-    const collection = db.collection('name');
-    collection.insert({'name': 'value'});
-  });
-
-  res.status(200).end();
-  db.close();
 });
 
 const server = app.listen(3000, function () {
